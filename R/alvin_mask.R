3d58cb59ce9527c0b23b40ba41b3a34cc6b3ade4
muschellij2 <muschellij2@gmail.com>
2020-01-05 17:41:12 -0500

    submit_package_release fixed bug in different resolution

--- R/alvin_mask.R
+++ R/alvin_mask.R
@@ -9,12 +9,19 @@
 #' alvin_mask(mm = 1)
 #' alvin_mask(mm = 0.5)
 #' alvin_image()
+#' testthat::expect_equal(
+#' dim(alvin_image(1, read_data = FALSE)),
+#' c(182L, 218L, 182L))
+#' testthat::expect_equal(
+#' dim(alvin_image(0.5, read_data = FALSE)),
+#' c(364L, 436L, 364L))
 alvin_mask = function(mm = c("2", "1","0.5")) {
   mm = as.character(mm)
   mm = match.arg(mm)
   app = switch(
-    "1" = "_1",
-    "0.5" = "_0.5",
+    mm,
+    "1" = "_1mm",
+    "0.5" = "_0.5mm",
     "2" = ""
   )
   system.file(paste0("ALVIN_mask_v1", app, ".nii.gz"), package = "alvin")
